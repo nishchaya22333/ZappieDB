176c7ae49a255c0ded86dc7d129dd73acb11408f
Aditi Sharma <119067612+Aditi1409sharma@users.noreply.github.com>
2024-03-31 00:29:03 +0530

    Update db_creation.sql

--- server_end/db_creation.sql
+++ server_end/db_creation.sql
@@ -140,14 +140,4 @@ CREATE TABLE `Zappiedb`.`Adds_Product` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
     
-    -- Modifying databse to add a table cart to improve performance and ease of use.
-CREATE TABLE `Zappiedb`.`Cart` (
-  `Cart_ID` INT NOT NULL,
-  `Custom_ID` INT NOT NULL,
-  PRIMARY KEY (`Cart_ID`),
-  INDEX `Cust_ID_idx` (`Custom_ID` ASC) VISIBLE,
-  CONSTRAINT `Custom_ID`
-    FOREIGN KEY (`Custom_ID`)
-    REFERENCES `Zappiedb`.`Customer` (`Cust_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+   
