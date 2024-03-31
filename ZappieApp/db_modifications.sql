6c7fafe29594a2ad4d7e085c4f471be52bca7b29
Aditi Sharma <119067612+Aditi1409sharma@users.noreply.github.com>
2024-03-31 19:36:23 +0530

    Update db_modifications.sql

--- ZappieApp/db_modifications.sql
+++ ZappieApp/db_modifications.sql
@@ -1,4 +1,4 @@
--- Modifying databse to add a table cart to improve performance and ease of use.
+-- Modifying database to add a table cart to improve performance and ease of use.
 CREATE TABLE `Zappiedb`.`Cart` (
   `Cart_ID` INT NOT NULL,
   `Custom_ID` INT NOT NULL,
@@ -8,11 +8,14 @@ CREATE TABLE `Zappiedb`.`Cart` (
     FOREIGN KEY (`Custom_ID`)
     REFERENCES `Zappiedb`.`Customer` (`Cust_ID`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION
+);
 
 USE Zappiedb;
+
 DROP TABLE `Order_Products`;
--- Former adds_product table is modified to added_products in accordance to update database design.
+
+-- Former adds_product table is modified to added_products in accordance with the updated database design.
 ALTER TABLE `adds_product` DROP Constraint `Customer_ID`;
 ALTER TABLE `adds_product` DROP COLUMN `Customer_ID`;
 ALTER TABLE  `adds_product` ADD COLUMN `Cart_ID` INT;
@@ -39,8 +42,7 @@ REFERENCES `Zappiedb`.`Cart`(`Cart_ID`);
 -- ALTER TABLE `added_products` DROP Constraint `Prd_ID`;
 ALTER TABLE `added_products` DROP PRIMARY KEY;
 
-
--- Renaming columns to ensure unform naming convention to facilitate smooth query calls from server-end programs.
+-- Renaming columns to ensure uniform naming convention to facilitate smooth query calls from server-end programs.
 ALTER TABLE Cart
 RENAME COLUMN Custom_ID TO Cust_ID;
 
@@ -53,16 +55,14 @@ RENAME COLUMN `Stor_ID` TO `Store_ID`;
 ALTER TABLE `added_products` 
 RENAME COLUMN `Prd_ID` TO `Prod_ID`;
 
-
 ALTER TABLE `customer` 
 ADD COLUMN `Password` varchar(400) NOT NULL;
 
-AlTER TABLE `cart` 
+ALTER TABLE `cart` 
 ADD COLUMN `date_time` DATETIME NOT NULL;
 
--- trigger creation 
+-- Trigger creation 
 
-DELIMITER //
 CREATE TRIGGER update_availability_on_product_removal
 AFTER DELETE ON added_products
 FOR EACH ROW
@@ -70,33 +70,20 @@ BEGIN
     DECLARE prod_quantity INT;
     SELECT Quantity INTO prod_quantity FROM Availability WHERE Prod_ID = OLD.Prod_ID;
     UPDATE Availability SET Quantity = prod_quantity + OLD.Quantity WHERE Prod_ID = OLD.Prod_ID;
-END//
-DELIMITER ;
+END;
 
-
-DELIMITER //
 CREATE TRIGGER update_availability_after_add
 AFTER INSERT ON added_products
 FOR EACH ROW
 BEGIN
     DECLARE available_quantity INT;
+    SELECT Quantity INTO available_quantity FROM Availability WHERE Prod_ID = NEW.Prod_ID;
+    UPDATE Availability SET Quantity = available_quantity - NEW.Quantity WHERE Prod_ID = NEW.Prod_ID;
+END;
 
-    -- Get the current available quantity for the product
-    SELECT Quantity INTO available_quantity
-    FROM Availability
-    WHERE Prod_ID = NEW.Prod_ID;
-
-    -- Update the availability
-    UPDATE Availability
-    SET Quantity = available_quantity - NEW.Quantity
-    WHERE Prod_ID = NEW.Prod_ID;
-END//
-DELIMITER ;
-
-DELIMITER //
-CREATE TRIGGER set_delivery_date_time BEFORE INSERT ON `Order`
+CREATE TRIGGER set_delivery_date_time
+BEFORE INSERT ON `Order`
 FOR EACH ROW
 BEGIN
     SET NEW.DeliveryDateTime = NULL;
-END //
-DELIMITER ;
+END;
