d896270838ac8427b295523f3acfd9d4cb493e5f
Aditi Sharma <119067612+Aditi1409sharma@users.noreply.github.com>
2024-03-31 18:42:53 +0530

    Update db_modifications.sql

--- ZappieApp/db_modifications.sql
+++ ZappieApp/db_modifications.sql
@@ -59,3 +59,37 @@ ADD COLUMN `Password` varchar(400) NOT NULL;
 
 AlTER TABLE `cart` 
 ADD COLUMN `date_time` DATETIME NOT NULL;
+
+-- trigger creation 
+CREATE TRIGGER update_availability_on_product_removal
+AFTER DELETE ON added_products
+FOR EACH ROW
+BEGIN
+    DECLARE prod_quantity INT;
+    SELECT Quantity INTO prod_quantity FROM Availability WHERE Prod_ID = OLD.Prod_ID;
+    UPDATE Availability SET Quantity = prod_quantity + OLD.Quantity WHERE Prod_ID = OLD.Prod_ID;
+END;
+
+CREATE TRIGGER update_availability_after_add
+AFTER INSERT ON added_products
+FOR EACH ROW
+BEGIN
+    DECLARE available_quantity INT;
+
+    -- Get the current available quantity for the product
+    SELECT Quantity INTO available_quantity
+    FROM Availability
+    WHERE Prod_ID = NEW.Prod_ID;
+
+    -- Update the availability
+    UPDATE Availability
+    SET Quantity = available_quantity - NEW.Quantity
+    WHERE Prod_ID = NEW.Prod_ID;
+END;
+
+CREATE TRIGGER set_delivery_date_time
+BEFORE INSERT ON orders
+FOR EACH ROW
+BEGIN
+    SET NEW.DeliveryDateTime = NULL;
+END;
