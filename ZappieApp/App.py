2120c6820e5256f45e1b422270ada5819b9cd08f
nishchaya22333 <132191677+nishchaya22333@users.noreply.github.com>
2024-04-19 20:35:22 +0530

    Update App.py

--- ZappieApp/App.py
+++ ZappieApp/App.py
@@ -1,41 +1,31 @@
+import pyfiglet
+from pyfiglet import Figlet
 import art
-import GUI_boot
-import getpass
-import admin_funtions
+import admin_menu
+import customer_menu
 
-def admin_access():
-    # print("ok")
-    choice = input("1. Add store \n2. Add Employee \n3. Get Store Analysis \n4. Get Employee Analysis \nEnter your choice: ")
-    if (choice == "1"):
-        admin_funtions.registerStore()
-    elif (choice == "2"):
-        admin_funtions.registerEmployee()
-    elif (choice == "3"):
-        admin_funtions.getStoreAnalysis()
-    elif (choice == "4"):
-        admin_funtions.getEmployeeAnalysis()
-    else:
-        print(art.traffic)
-        print("Invalid Choice.")
+# Text color
+colour = ["\033[94m",'\033[92m', "\033[91m"] #blue, green, red
+colour_end = "\033[0m"
 
-def main():
-    choice = input("1. Boot server and create database \n2. Admin LogIn \n3. Seller LogIn \n4. Employee LogIn \nEnter your choice: ")
-    if (choice == "1"):
-        warning_message = "Proceed only if server is in initial state. Attempt to duplicate database will lead to termination."
-        print("\u26A0"+"\033[91mWarning: \033[0m" + " " + warning_message)
-        proceed = int(input("1. Continue\n2. Cancel\nEnter your choice: "))
-        if proceed == 1:
-            GUI_boot.main()
-    elif (choice == "2"):
-        x = input("Enter username: ")
-        y = getpass.getpass("Enter password: ")
-        if(x == "admin" and y == "admin"):
-            admin_access()
-    elif (choice == "3"):
-        pass
-    elif (choice == "4"):
-        pass
+
+# Welcome text
+f = pyfiglet.figlet_format("Zappie", font="slant")
+print(art.cow)
+print("        ____________________")
+print(colour[0], f,colour_end)
+print(colour[1] + "            Welcome" + colour_end)
+
+print("Choose an option to proceed: ")
+while(True):
+    choice = (input("1. Open Admin Portal \n2. Open Customer Portoal\n3. Exit \nEnter you choice: "))
+    if choice == "1":
+        admin_menu.main()
+        continue
+    elif choice == "2":
+        customer_menu.main()
+    elif choice == "3":
+        break
     else:
-        print(art.dog)
-        print("Invalid Choice.")
-# main()
+        print(colour[2],"Invalid Choice", colour_end)
+        continue
